8bfd731920e4586822bfaab8b61f8d653c5f65c2
David Justo <david.justo.1996@gmail.com>
2022-10-18 10:35:59 -0700

    add InMemoryTransport abstraction

--- src/Orleans.Core/Networking/Shared/SharedMemoryPool.cs
+++ src/Orleans.Core/Networking/Shared/SharedMemoryPool.cs
@@ -2,7 +2,7 @@ using System.Buffers;
 
 namespace Orleans.Networking.Shared
 {
-    internal sealed class SharedMemoryPool
+    public sealed class SharedMemoryPool // NOTE: made this public for testing
     {
         public MemoryPool<byte> Pool { get; } = KestrelMemoryPool.Create();
     }
